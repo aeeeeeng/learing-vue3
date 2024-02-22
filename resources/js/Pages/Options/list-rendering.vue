715a323e94b3558f8eccca3f8e60358ac358004b
syahril <aeeeeeng@gmail.com>
2024-02-22 14:36:15 +0700

    feat(ListRendering): list rendering using composition & options style

---
+++ resources/js/Pages/Options/list-rendering.vue
@@ -0,0 +1,39 @@
+<script>
+let id = 0
+export default {
+    data() {
+        return {
+            newTodo: '',
+            todos: [
+                {id: id++, text: 'learn HTML'},
+                {id: id++, text: 'learn Javascript'},
+                {id: id++, text: 'learn VUE'},
+            ]
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todos.push(
+                {id: id++, text: this.newTodo}
+            )
+            this.newTodo = ''
+        },
+        removeTodo(todo) {
+            this.todos = this.todos.filter(t => t !== todo)
+        }
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="addTodo">
+        <input type="text" v-model="newTodo">
+        <button>Add Todo</button>
+    </form>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.text }}
+            <button @click="removeTodo(todo)">X</button>
+        </li>
+    </ul>
+</template>
